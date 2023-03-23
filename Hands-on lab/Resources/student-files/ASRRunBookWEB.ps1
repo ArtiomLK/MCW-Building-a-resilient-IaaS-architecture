e47b6b0106f8cd58601d6951695c6ed85490cd92
Artiom Lezcano Koretski <koretskiartiom@gmail.com>
2023-03-22 22:47:35 -0500

    deploy BCDR IaC on second region with PS (#2)
    * deploy BCDR IaC on second region with PS
    
    * lint automation bcdr ps scripts

--- Hands-on lab/Resources/student-files/ASRRunBookWEB.ps1
+++ Hands-on lab/Resources/student-files/ASRRunBookWEB.ps1
@@ -1,44 +1,44 @@
-<# 
+<#
 Microsoft Cloud Workshop: BCDR
 .File Name
  - ASRRunBookWEB.ps1
 
 .What calls this script?
  - This is an Azure Automation Runbook used for Failing over and Failing Back the Web Servers Region to Region.
- 
+
  - Azure Site Recovery is required for this to function properly as it relies on the context,
    of the failover type passed.
 
-.What does this script do?  
- - When there is a Failover from Primary to Secondary the RecoveryPlanContext.FailoverDirection property 
+.What does this script do?
+ - When there is a Failover from Primary to Secondary the RecoveryPlanContext.FailoverDirection property
    is set to: "PrimaryToSecondary".
- 
+
  - The WEBVMs that failover to the Secondary Site will be added to the Backend Pool of the External Load Balancer.
 
- - When there is a Failback from Secondary to Primary the RecoveryPlanContext.FailoverDirection property 
+ - When there is a Failback from Secondary to Primary the RecoveryPlanContext.FailoverDirection property
    is set to: "SecondaryToPrimary".
- 
+
  - The WEBVMs that failback to the Primary Site will be added to the Backend Pool of the External Load Balancer.
 
 #>
 workflow ASRWEBFailover
 {
-    param ( 
-        [parameter(Mandatory=$false)] 
-        [Object]$RecoveryPlanContext 
-    ) 
-    $connectionName = "AzureRunAsConnection" 
-	    
-Try 
+    param (
+        [parameter(Mandatory=$false)]
+        [Object]$RecoveryPlanContext
+    )
+    $connectionName = "AzureRunAsConnection"
+
+Try
  {
     #Logging in to Azure...
 
     "Logging in to Azure..."
-    $Conn = Get-AutomationConnection -Name AzureRunAsConnection 
+    $Conn = Get-AutomationConnection -Name AzureRunAsConnection
      Add-AzAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
 
     "Selecting Azure subscription..."
-    Select-AzSubscription -SubscriptionId $Conn.SubscriptionID -TenantId $Conn.tenantid 
+    Select-AzSubscription -SubscriptionId $Conn.SubscriptionID -TenantId $Conn.tenantid
  }
 Catch
  {
@@ -49,19 +49,19 @@ Catch
       Write-Error -Message $ErrorMessage `
                     -ErrorAction Stop
  }
-	
+
     $RPVariable = Get-AutomationVariable -Name $RecoveryPlanContext.RecoveryPlanName
     $RPVariable = $RPVariable | convertfrom-json
-	
+
 	"Configurations used by this Runbook for the Failover..."
     Write-Output $RPVariable
 
 	"Determining if Failover or Failback..."
 	Write-Output $RecoveryPlanContext.FailoverDirection
-	
+
 	"Configuring Script based on Direction of Failover..."
-    if ($RecoveryPlanContext.FailoverDirection -eq "PrimaryToSecondary") { 
-		
+    if ($RecoveryPlanContext.FailoverDirection -eq "PrimaryToSecondary") {
+
 
         InlineScript {
             $RP = $Using:RPVariable
@@ -87,7 +87,7 @@ Catch
 		}
     }
     else {
-       
+
         InlineScript {
             $RP = $Using:RPVariable
 			$LBNAME   = $RP.PrimarySiteWebLBName
